ee218e07c6d4452b4a4b18f9037bd6be9e29ccfb
Henry Chen <chenx97@aosc.io>
2020-10-31 18:17:21 -0700

    Nav.css, Nav.js: fix width of nav bar
    otherwise, the width is 100% for the content. This means that if
    I have a window of width 500, the content will have a width of 500,
    and with both sides having a 20px padding it will have an actual
    width of 540, and the right hand side will go outside the page.
    
    The fix is to add a fixed wrapper with no border, padding or margin,
    and we will give it a width of 100%.
    Inside this wrapper, the original nav can exist with an automatic
    width. This allows the inner element to automatically compute its
    width.

--- movie-recommendation-system/src/Nav.js
+++ movie-recommendation-system/src/Nav.js
@@ -15,19 +15,21 @@ function Nav() {
     };
   }, []);
   return (
-    <div className={`nav ${show && "nav__black"}`}>
-      <img
-        className="nav__logo"
-        src="https://bendblockbuster.com/wp-content/themes/blockbuster/assets/images/blockbuster-logo.svg"
-        alt="BlockBuster Logo"
-      />
+    <div className="nav__wrapper">
+      <div className={`nav ${show && "nav__black"}`}>
+        <img
+          className="nav__logo"
+          src="https://bendblockbuster.com/wp-content/themes/blockbuster/assets/images/blockbuster-logo.svg"
+          alt="BlockBuster Logo"
+        />
 
-      <img
-        className="nav__avatar"
-        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
-        alt="BlockBuster Logo"
-      />
-    </div>
+        <img
+          className="nav__avatar"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+          alt="BlockBuster Logo"
+        />
+      </div>
+    </div >
   );
 }
 
