13a99d2841f44c65e89fb2d00442f1d5f98006e1
kotran222 <kobetrankhoi222@gmail.com>
2020-11-07 00:42:57 -0800

    still in progress of login

--- movie-recommendation-system/src/App.js
+++ movie-recommendation-system/src/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+// import { observer } from 'mobx-react';
+// import UserStore from './stores/UserStore';
+// import LoginForm from './LoginForm';
+// import SubmitButton from './SubmitButton';
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
@@ -6,27 +10,119 @@ import Banner from "./Banner";
 import Nav from "./Nav";
 import Footer from "./Footer";
 
-function App() {
-  return (
-    <div className="app">
-      {/* Nav */}
-      <Nav />
+function App(){
+// class App extends React.Component {
+  // async componentDidMount(){
+  //   try{
+  //     let res = await fetch('/isLoggedIn', {
+  //       method: 'post',
+  //       headers: {
+  //         'Accept': 'application/json',
+  //         'Content-Type': 'application/json'
+  //       }
+  //     });
 
-      {<Banner />}
+  //     let result = await res.json();
 
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-      <Row title="Western" fetchUrl={requests.fetchWesternMovies} />
-      <Row title="Family" fetchUrl={requests.fetchFamily} />
+  //     if(result && result.success){
+  //       UserStore.loading = false;
+  //       UserStore.isLoggedIn = true;
+  //       UserStore.username = result.username;
+  //     }
+  //     else{
+  //       UserStore.loading = false;
+  //       UserStore.isLoggedIn = false;
+  //     }
+  //   }
+  //   catch(e){
+  //       UserStore.loading = false;
+  //       UserStore.isLoggedIn = false;
+  //   }
+  // }
 
-      <Footer />
-    </div>
-  );
+  // // Logout Function
+  // async doLogout(){
+  //   try{
+  //     let res = await fetch('/logout', {
+  //       method: 'post',
+  //       headers: {
+  //         'Accept': 'application/json',
+  //         'Content-Type': 'application/json'
+  //       }
+  //     });
+
+  //     let result = await res.json();
+
+  //     if(result && result.success){
+  //       UserStore.isLoggedIn = false;
+  //       UserStore.username = '';
+  //     }
+  //   }
+  //   catch(e){
+  //     console.log(e);
+  //   }
+  // }
+
+  // render(){
+  //   if(UserStore.loading){
+  //     return(
+  //       <div className="app">
+  //         <div className="container">
+  //           Loading, please wait...
+  //         </div>
+  //       </div>
+  //     );
+  //   }
+
+  //   else{
+  //     if(UserStore.isLoggedIn){
+  //       <div className="app">
+  //         <div className="container">
+  //           Welcome{UserStore.username}
+  //           <SubmitButton
+  //             text={'Log out'}
+  //             disabled={false}
+  //             onClick={ () => this.doLogout() }
+  //           />
+  //         </div>
+  //       </div>
+  //     }
+  //     return(
+  //       <div className="app">
+  //           <div className='container'>
+  //             {/* <SubmitButton
+  //               text={'Log out'}
+  //               disabled={false}
+  //               onClick={ () => this.doLogout() }
+  //             /> */}
+  //             <LoginForm/>
+  //           </div>
+  //       </div>
+  //     );
+  //   }
+
+    return (
+        <div className="app">
+          {/* Nav */}
+          <Nav />
+
+          {<Banner />}
+
+          <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
+          <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+          <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+          <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+          <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+          <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+          <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+          <Row title="Western" fetchUrl={requests.fetchWesternMovies} />
+          <Row title="Family" fetchUrl={requests.fetchFamily} />
+
+          <Footer />
+        </div>
+    );
+  // }
 }
 
 export default App;
+// export default observer(App);
