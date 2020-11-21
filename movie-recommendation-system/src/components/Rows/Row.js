3d37e590e3a71c7a64fcc30a9f1089f7a4ea218c
vincentyu11 <39112951+vincentyu11@users.noreply.github.com>
2020-11-20 23:12:22 -0800

    popup implementation (not complete)

--- movie-recommendation-system/src/components/Rows/Row.js
+++ movie-recommendation-system/src/components/Rows/Row.js
@@ -3,6 +3,10 @@ import axios from "../../axios"; //axios is an alias here when importing. could 
 import "./Row.css";
 import YouTube from "react-youtube";
 import movieTrailer from "movie-trailer";
+import Button from 'react-bootstrap/Button';
+import MovieContent from "../Content/Content";
+import Modal from "react-bootstrap/Modal";
+import "bootstrap/dist/css/bootstrap.min.css"; 
 
 const base_url = "https://images.tmdb.org/t/p/original/";
 
@@ -32,8 +36,10 @@ function Row({ title, fetchUrl, isLargeRow }) {
     },
   };
 
+
   // NEED TO TEST THIS SOMETHING IS WRONG
   const handleClick = (movie) => {
+    /*
     if (trailerUrl) {
       setTrailerUrl("");
     } else {
@@ -44,8 +50,23 @@ function Row({ title, fetchUrl, isLargeRow }) {
         })
         .catch((error) => console.log(error));
     }
+    */
+
+    //console.log(movie.title);
   };
 
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const showModal = (movie) => {
+    setIsOpen(true);
+  };
+
+  const hideModal = () => {
+    setIsOpen(false);
+  };
+
+
+  //NEEDS FIXING
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -55,17 +76,29 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {/* several row__posters(s) */}
 
         {movies.map((movie) => (
+          <>
           <img
+            title={movie.title}
             key={movie.id}
-            onClick={() => handleClick(movie)}
+            onClick={() => showModal(movie)}
+            //onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path
               }`}
             alt={movie.name}
           />
+          <Modal show={isOpen} onHide={hideModal}>
+            <Modal.Header>{movie.title}</Modal.Header>
+            <Modal.Body>{movie.overview}</Modal.Body>
+            <Modal.Footer>
+              <button onClick={hideModal}>Exit</button>
+            </Modal.Footer>
+          </Modal>
+          </>
         ))}
       </div>
       {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+  
     </div>
   );
 }
