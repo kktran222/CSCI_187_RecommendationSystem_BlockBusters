b7123e8c8e6d49eaba7c50e6507a8bc06cee4ee7
vincentyu11 <39112951+vincentyu11@users.noreply.github.com>
2020-11-24 17:38:06 -0800

    Revert "Merge branch 'vincent_branch' into henry_popup"
    This reverts commit f907cc45450a425f1a85995abc89b6701d7d3e3f, reversing
    changes made to fcf8f6743f0b4f8447ad7c2e9fba5f0d4485fa2b.

--- movie-recommendation-system/src/App.js
+++ movie-recommendation-system/src/App.js
@@ -1,36 +1,39 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from "./components/LandingPages/Home/Home";
-import LoginPage from './components/LandingPages/Login/LoginPage';
-import Content from './components/UserPages/Content/Content';
-import ContentTV from './components/UserPages/Content/ContentTV';
+import LoginPage from './components/Login/LoginPage';
+import Content from './components/Content/Content';
+import ContentTV from './components/Content/ContentTV';
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import MyList from "./components/UserPages/MyList/MyList";
+import MyList from "./components/MyList/MyList";
 
 function App() {
 
   return (
     <Router>
       <div className="app">
+        <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/home" exact component={Home} />
-          <Route path="/login" exact component={LoginPage} />
-          {/* <Route path="/logout" exact component={Home} />
+          <Route path="/myList" component={MyList}></Route>
           <Route path="/content" component={Content}></Route>
           <Route path="/contenttv" component={ContentTV}></Route>
-          <Route path="/myList" component={MyList}></Route> */}
         </Switch>
-
         <Footer />
       </div>
     </Router>
-
-
   );
 }
+
+const Home = () => (
+  <Router>
+    <div>
+      <Route path="/" component={LoginPage}></Route>
+    </div>
+  </Router>
+
+);
 
 export default App;
 
