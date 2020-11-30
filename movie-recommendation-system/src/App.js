003a5d402c0ba3d7f49d5b1b38a01d5b45b189ef
kotran222 <kobetrankhoi222@gmail.com>
2020-11-30 12:15:48 -0800

    don't merge

--- movie-recommendation-system/src/App.js
+++ movie-recommendation-system/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginPage from './components/Login/LoginPage';
 import Content from './components/Content/Content';
--- movie-recommendation-system/src/components/UserPages/User.js
+++ movie-recommendation-system/src/components/UserPages/User.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "../../App.css";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from "../LandingPages/Home/Home";
 import LoginPage from '../LandingPages/Login/LoginPage';
