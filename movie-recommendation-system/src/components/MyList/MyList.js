c21337cb47d7367a5843ac28cf5ed448934a2ec9
sam-barba <73370795+sam-barba@users.noreply.github.com>
2020-12-01 21:38:54 -0800

    change mylist to component
    change from function to component so it renders properly, still need to fix so it loads correctly when first clicked.

--- movie-recommendation-system/src/components/MyList/MyList.js
+++ movie-recommendation-system/src/components/MyList/MyList.js
@@ -6,19 +6,19 @@ import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "../../axios";
 import '../Rows/Row.css';
-
+import Button from 'react-bootstrap/Button';
 
 
 async function getUserMovies(userID, listID) {
-  var list = [];
+  var temp = [];
   await firebase.database().ref('/saved/' + userID + '/' + listID + '/').once('value').then((snapshot) => {
     snapshot.forEach((i) => {
-      list.push(i.val().ID)
+      temp.push(i.val().ID)
     });
 
   });
-  console.log(list)
-  return list;
+  console.log(temp)
+  return temp;
 };
 
 const base_url = "https://images.tmdb.org/t/p/w92/";
@@ -47,16 +47,38 @@ async function getResponse(reqs) {
   return ret;
 }
 
-function MyList() {
-  const [userID, setUserID] = useState(firebaseD.auth().currentUser.uid);
-  const [list, setList] = useState([]);
 
-  useEffect(() => {
-    //Notes:
-    //if blank [], run once when the row loads, and don't run again
-    //dependencies: if [] contains a variable (e.g. movies), run once when row loads, and then run everytime when movies changes.
+async function removeFromList(movie, userID, listID) {
+    var dbref = firebase.database().ref('/saved/' + userID + '/' + listID + '/')
+    var marked = []
+    await dbref.once('value').then((snapshot) => {
+        snapshot.forEach((i) => {
+        console.log(movie)
+        console.log(i.val().ID)
+        console.log(i.ref)
+        if (i.val().ID == movie) {
+            i.ref.set(null)
+        }
+        });
+});
 
-    async function fetchData() {
+};
+// function MyList() {
+class MyList extends React.Component {
+   constructor(props){
+          super(props);
+          this.state={userID: firebaseD.auth().currentUser.uid, list: [], refresh: 0, show: false, mid: null};
+   }
+
+
+  //const [userID, setUserID] = useState(firebaseD.auth().currentUser.uid);
+  //const [list, setList] = useState([]);
+  //const [refresh, setRefresh] = useState(0);
+  
+
+  async componentDidMount(prevProps){
+
+    /*async function fetchData() {
       setUserID(firebaseD.auth().currentUser.uid);
       var temp = await getUserMovies(userID, 1);
       var myRequests = [];
@@ -70,88 +92,91 @@ function MyList() {
       await console.log(list);
 
       return list;
-    }
+    }*/
+
+        console.log()
+        var temp = await getUserMovies(firebaseD.auth().currentUser.uid, 1);
+        console.log(temp)
+        var myRequests = [];
+        temp.map((id) => {
+            myRequests.push('https://api.themoviedb.org/3/movie/' + (id) + '?api_key=1be335fcb8ba9c525f9b9bd2124294d6&language=en-US')
+        });
+        const x = await getResponse(myRequests);
+        console.log(x);
+        await this.setState({list: x})
+        console.log(this.state.list)
+        this.forceUpdate()
+     
+  } 
 
 
-
-    fetchData();
-  }, [setList]);
-  const final = list;
-  console.log(final);
-
-  const [isOpen, setIsOpen] = React.useState(false);
-  const [modalMovieID, setModalID] = React.useState(null);
-
-  const showModal = (movie) => {
-    setIsOpen(true);
-    setModalID(movie.id);
+  showModal = (movie) => {
+    this.setState({open: true, mid: movie.id});
     console.log(movie.title + 'has been inspected')
   };
-
-  const hideModal = () => {
-    setIsOpen(false);
+  removeMovie = (movie) => {
+        var temp = this.state.list;
+        console.log(movie.id)
+        console.log(temp[0].id)
+        temp = temp.filter((x) => movie.id != x.id ) 
+        removeFromList(movie.id, this.state.userID, 1);
+        console.log(temp)
+        this.setState({list: temp})
+        this.forceUpdate()
+  }
+  hideModal = () => {
+    this.setState({open: false});
   };
+  render() {
+  console.log(this.state.refresh)
+  console.log(this.state.list)
+      return (
+        <div ref={React.createRef()} className="myList">
 
-  async function removeFromList(movie, userID, listID) {
-    var dbref = firebase.database().ref('/saved/' + userID + '/' + listID + '/')
-    var marked = []
-    await dbref.once('value').then((snapshot) => {
-      snapshot.forEach((i) => {
-        console.log(movie)
-        console.log(i.val().ID)
-        console.log(i.ref)
-        if (i.val().ID == movie) {
-          i.ref.set(null)
+          <h1 className="myList-title">{'MyList'}</h1>
 
+          {/* container -> posters */}
+          <div className="my_row__posters">
+            {/* several row__posters(s) */}
 
-        }
-      });
-    });
-    marked.map((j) => dbref + j)
-
-  };
-
-  return (
-    <div className="myList">
-
-      <h1 className="myList-title">{'MyList'}</h1>
-
-      {/* container -> posters */}
-      <div className="my_row__posters">
-        {/* several row__posters(s) */}
-
-        {final.map((movie) => (
-
-          <>
-            <img
-              title={movie.title}
-              key={movie.id}
-              onClick={() => showModal(movie)}
-              //onClick={() => handleClick(movie)}
-              className={`my_row__poster`}
-              src={`${base_url}${movie.poster_path}`}
-              alt={movie.name}
-            />
-            <Modal show={modalMovieID === movie.id && isOpen}
-              onHide={hideModal}
-              className="row__modal">
-              <Modal.Header>{movie.title}</Modal.Header>
-              <Modal.Body>{movie.overview}</Modal.Body>
-              <Modal.Footer>
-                <button onClick={hideModal}>Exit</button>
-                <button onClick={() => removeFromList(movie.id, userID, 1)}>Remove from MyList</button>
-              </Modal.Footer>
-            </Modal>
-          </>
+            {this.state.list.map((movie) => (
+        
+              <>
+                <img
+                  title={movie.title}
+                  key={movie.id}
+                  onClick={() => this.showModal(movie)}
+                  //onClick={() => handleClick(movie)}
+                  className={`my_row__poster`}
+                  src={`${base_url}${movie.poster_path}`}
+                  alt={movie.name}
+                />
+                <Modal show={this.state.mid === movie.id && this.state.open}
+                  onHide={this.hideModal}
+                  className="row__modal">
+                  <Modal.Header>{movie.title}</Modal.Header>
+                  <Modal.Body>{movie.overview}</Modal.Body>
+                  <Modal.Footer>
+                    <button onClick={this.hideModal}>Exit</button>
+                    <button onClick={() => 
+                        this.removeMovie(movie)
+                    }>Remove from MyList</button>
+                  </Modal.Footer>
+                </Modal>
+              </>
 
 
 
 
-        ))}
-      </div>
-
-    </div>
-  );
+            ))}
+          </div>
+          <Button variant="light" onClick={()=>this.setState({refresh: this.state.refresh+1})}>refresh</Button>
+          <br />
+          <br />
+          <br />
+        </div>
+      );
+  }
 }
 
 
